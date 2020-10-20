fdf50c6636097b81b6016d684240ed51d0d6f588
Patrick Gichuki <patmwagic@gmail.com>
2020-10-20 19:24:00 +0300

    seeding item type, item sub type, and item master for drugs

--- Moms.Revenue.Core/Domain/Item/Models/ItemMaster.cs
+++ Moms.Revenue.Core/Domain/Item/Models/ItemMaster.cs
@@ -9,7 +9,7 @@ namespace Moms.Revenue.Core.Domain.Item.Models
     {
         public string  Name { get; set; }
         public string  DisplayName { get; set; }
-        public Guid ItemTypeId { get; set; }
+        public Guid? ItemTypeId { get; set; }
         public Guid ItemSubTypeId { get; set; }
         public string ItemCode { get; set; }
         public string ItemDescription { get; set; }
